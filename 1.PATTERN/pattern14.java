ec8d8974d6617dcee38ace4993217c0cc5cb1dc0
SAMARTH-400 <91251982+SAMARTH-400@users.noreply.github.com>
2021-10-18 01:12:12 +0530

    Add files via upload

---
+++ 1.PATTERN/pattern14.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class pattern14 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1 ; i<=10 ; i++ ){
+            int value = n*i;
+            System.out.println(n+" * "+i+" = "+value);
+        }
+    }
+}
